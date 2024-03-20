846b1be37ea113cb4f99a5eaa7bbe3337dbfb8dc
Pasindu Sathsara Rasanga <155545637+pasindu-22@users.noreply.github.com>
2024-03-20 17:54:44 +0530

    Update POS.java

--- POS.java
+++ POS.java
@@ -1,62 +1,69 @@
+
 import java.io.*;
 import java.util.*;
 
 
 public class POS implements Serializable{
-    Database db = Database.getInstance();
-    private ArrayList<Cashier> cashiers = new ArrayList<>(List.of(new Cashier("Saman",155,"null"),new Cashier("Osa",156,"null")));
-    private ArrayList<Customer> customers = new ArrayList<>(List.of(new Customer("Pasindu",055,"null"),new Customer("Ravindu",056,"null")));
-    private String branch = "Galle";
-    private ArrayList<Bill> pendingBills = new ArrayList<>();
-    private ArrayList<Bill> completedBills = new ArrayList<>();
+    static Database db = Database.getInstance();
+    private static ArrayList<Cashier> cashiers = new ArrayList<>(List.of(new Cashier("Saman",155,"null"),new Cashier("Osa",156,"null")));
+    private static ArrayList<Customer> customers = new ArrayList<>(List.of(new Customer("Pasindu",055,"null"),new Customer("Ravindu",056,"null")));
+    private static String branch = "Galle";
+    private static ArrayList<Bill> pendingBills = new ArrayList<>();
+    private static ArrayList<Bill> completedBills = new ArrayList<>();
 
-    public POS() {
-        db.createDb();
-    }
-
-    public GloceryItem GetItemDetails() {
+    public static GloceryItem getItemDetails() throws ExitException{
         GloceryItem item = null;
         try {
             InputStreamReader r = new InputStreamReader(System.in);
             BufferedReader br = new BufferedReader(r);
             String item_code = br.readLine();
+            if (item_code.equals("n")) {
+                throw new ExitException();
+            }
             item = db.getItem(Integer.parseInt(item_code));
 
         } catch (ItemCodeNotFound e) {
-            System.out.println("Wrong item!");
+            System.out.println("Invalid Item code!");
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Invalid Item code!");
         }
         return item;
     }
 
 
 
-    private static void createBill(POS store, Bill b) {
+    private static void createBill(Bill b) {
         Scanner sc = new Scanner(System.in);
         while (true) {
-            System.out.println("Enter item code: ");
-            GloceryItem item = store.GetItemDetails();
+            System.out.println("Enter item code: (Enter 'n' to exit)");
+            GloceryItem item;
+            try {
+                 item = getItemDetails();
+            } catch (ExitException e) {
+                break;
+            }
             if (item==null) {
                 continue;
             }
             System.out.println("Enter quantity:");
             double quantity = sc.nextDouble();
             b.addItem(item,quantity);
-            System.out.println("Do you want to add more items? (y/n)");
-            String more = sc.next();
-            if (more.equals("n")) {
-                b.setTotal();
-                b.setTotalDiscount();
-                break;
-            }
+
+            //            System.out.println("Do you want to add more items? (y/n)");
+//            String more = sc.next();
+//            if (more.equals("n")) {
+//                b.setTotal();
+//                b.setTotalDiscount();
+//                break;
+//            }
         }
+
         System.out.println("Do you want to print the bill? (y/n)");
         String print = sc.next();
         if (print.equals("y")) {
             try {
                 b.printBill();
-                store.completedBills.add(b);
+                completedBills.add(b);
             } catch (NullPointerException e) {
                 System.out.println("No items found in the cart");
             }
@@ -64,11 +71,11 @@ public class POS implements Serializable{
         System.out.println("Do you want to add this bill to pending list? (y/n)");
         String pending = sc.next();
         if (pending.equals("y")) {
-            store.pendingBills.add(b);
+            pendingBills.add(b);
         }
     }
 
-    public void saveBill(){
+    public static void saveBill(){
         try{
             FileOutputStream fs = new FileOutputStream("Pendings.dat");
             ObjectOutputStream os = new ObjectOutputStream(fs);
@@ -79,7 +86,7 @@ public class POS implements Serializable{
         }
     }
 
-    public ArrayList<Bill> getBills() {
+    public static ArrayList<Bill> getBills() {
         try {
             FileInputStream fs = new FileInputStream("Pendings.dat");
             ObjectInputStream os = new ObjectInputStream(fs);
@@ -94,7 +101,7 @@ public class POS implements Serializable{
         }
     }
 
-    public Customer getCustomer(String name) throws CustomerNotFound{
+    public static Customer getCustomer(String name) throws CustomerNotFound{
         for (Customer c : customers) {
             if (c.getName().equalsIgnoreCase(name)) {
                 return c;
@@ -104,11 +111,11 @@ public class POS implements Serializable{
     }
 
     public static void main(String[] args) {
-        POS store = new POS();
+        db.createDb();
         System.out.println("Enter Cashier Name:");
         Scanner sc = new Scanner(System.in);
         String cashier = sc.nextLine();
-        store.pendingBills = store.getBills();
+        pendingBills = getBills();
         while (true) {
             System.out.println("Cashier Name: " + cashier);
             System.out.println("1.New bill");
@@ -127,34 +134,34 @@ public class POS implements Serializable{
                 String customer = sc.next();
                 boolean registered = false;
                 try {
-                    Customer c = store.getCustomer(customer);
+                    Customer c = getCustomer(customer);
                 } catch (CustomerNotFound e) {
                     System.out.println("Customer not found. Do you want to register? (y/n)");
                     String reg = sc.next();
                     if (reg.equals("y")) {
-                        store.customers.add(new Customer(customer,store.customers.size()+1,"null"));
+                        customers.add(new Customer(customer, customers.size()+1,"null"));
                     }
                 }
                 Bill bill = new Bill(cashier,customer);
-                createBill(store, bill);
+                createBill(bill);
             } else if (choice==2) {
                 Bill bill=null;
                 int option=0;
-                System.out.println("Bill number" + "    Cashier " + "      Customer Name "+ "      Total " );
+                System.out.println("BillNumber" + "    Cashier " + "      Customer Name "+ "      Total " );
                 int billNumber = 1;
-                for (Bill b : store.pendingBills) {
-                    System.out.println(billNumber + b.cashierName + b.customerName + b.getTotal());
+                for (Bill b : pendingBills) {
+                    System.out.printf("%5d  %12s  %15s  %15.2f \n",billNumber, b.cashierName ,b.customerName,b.getTotal());
                     billNumber++;
                 }
                 System.out.println("Enter the Bill number that you want to continue or 0 to exit:" );
                 option = sc.nextInt();
 
-                if (0 < option && option <= store.pendingBills.size()) {
-                    bill = store.pendingBills.get(option-1);
+                if (0 < option && option <= pendingBills.size()) {
+                    bill = pendingBills.get(option-1);
                     try {
-                        createBill(store, bill);
-                        store.completedBills.add(bill);
-                        store.pendingBills.remove(bill);
+                        createBill(bill);
+                        completedBills.add(bill);
+                        pendingBills.remove(bill);
                     } catch (NullPointerException e) {
                         System.out.println("No items found in the cart");
                     } catch (ConcurrentModificationException e) {
@@ -165,13 +172,10 @@ public class POS implements Serializable{
                 }
 
             } else if (choice==3) {
-                store.saveBill();
-                System.out.println("Good Bye!");
+                saveBill();
                 break;
             }
 
         }
     }
 }
-
-
